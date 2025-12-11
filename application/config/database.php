ddbf8fcfbad13741bbdb6a2b33bea121eb3c7ed7
Clark Steven <edongclarksteven@gmail.com>
2025-12-11 15:59:27 +0800

    n

--- application/config/database.php
+++ application/config/database.php
@@ -15,7 +15,7 @@ $db['default'] = array(
 	// 'database' => 'srmsportal_vmc',
 	// 'database' => 'srmsportal_vmc1',
 	// 'database' => 'srmsportal_demo2',
-	'database' => 'depedmis_journal',
+	'database' => 'deped_journal',
 	// 'database' => 'knpsrms',
 	// 'database' => 'knpsrms_db',
 
